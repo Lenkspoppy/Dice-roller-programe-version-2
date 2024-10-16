affc9944b5321ef9a28a004ea0e85771f8973180
lenkspoppy <real.lenkspoppy@gmail.com>
2024-10-16 13:22:11 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,20 +1,54 @@
 
+const display = document.getElementById("display");
+let startTime = 0;
+let timer = null;
+let elapsedTime = 0;
+let isRunning = false;
 
-function rollDice(){
-    const numOfDice = document.getElementById("numOfDice").value;
-    const diceResult = document.getElementById("diceResult");
-    const diceImages = document.getElementById("diceImages");
-    const values = [];
-    const images = [];
-    
-    for(let i = 0; i < numOfDice; i++){
-        const value = Math.floor(Math.random() * 6) + 1;
-        values.push(value);
-        images.push(`<img src = "Dice-images/${value}.webp">`);
+function start(){
+    if(!isRunning){
+        startTime = Date.now() - elapsedTime
+        timer = setInterval(update, 10)
+        isRunning = true;
 
     }
-
-    diceResult.textContent = `dice:${values.join(', ')}`
-    diceImages.innerHTML = images.join('');
- 
 }
+
+function stop(){
+    if(isRunning){
+        clearInterval(timer);
+        isRunning = false;
+    }
+}
+
+function reset(){
+    startTime = 0;
+    clearInterval(timer);
+    elapsedTime = 0;
+    
+    isRunning = false;
+    display.textContent = "00:00:00:00"
+        
+}
+
+function update(){
+    const currentTime = Date.now()
+    elapsedTime = currentTime - startTime
+
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60 ))
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60 )
+    let seconds = Math.floor(elapsedTime / 1000 % 60)
+    let milliseconds = Math.floor(elapsedTime % 1000 /10)
+
+    hours = String(hours).padStart(2, "0")
+    minutes = String(minutes).padStart(2, "0")
+    seconds = String(seconds).padStart(2, "0")
+    milliseconds = String(milliseconds).padStart(2, "0")
+    
+    display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`
+    
+    
+
+
+}
+
